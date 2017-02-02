3281525eeaa546f08f8544602979c37befab3c24
defat <defat@ukr.net>
2017-02-02 13:51:49 +0200

    Version 0.0.4

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='simplejson_objects',
-    version='0.0.3',
+    version='0.0.4',
     py_modules=['simplejson_objects'],
     zip_safe=False,
     url='https://github.com/CrytekKiev/simplejson_objects',
-    download_url='https://github.com/CrytekKiev/simplejson_objects/archive/0.0.3.tar.gz',
+    download_url='https://github.com/CrytekKiev/simplejson_objects/archive/0.0.4.tar.gz',
     entry_points={
         'kombu.serializers': [
             'simplejson_objects = simplejson_objects:register_args'
