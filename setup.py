26fb54ba6646e65516ecae56ef020b6af7480f52
defat <defat@ukr.net>
2017-02-02 13:36:47 +0200

    install_requires removed as it's possible to fallback to default json module

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     version='0.0.3',
     py_modules=['simplejson_objects'],
     zip_safe=False,
-    install_requires=['simplejson'],
     url='https://github.com/CrytekKiev/simplejson_objects',
     download_url='https://github.com/CrytekKiev/simplejson_objects/archive/0.0.3.tar.gz',
     entry_points={
